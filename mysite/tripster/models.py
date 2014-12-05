2f77199d1f965a7c1c3cb93893b39087898d1ab8
Kevin <luhkevin@gmail.com>
2014-12-04 20:52:47 -0500

    models done

--- mysite/tripster/models.py
+++ mysite/tripster/models.py
@@ -3,45 +3,48 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class TripsterUser(models.Model)
+class TripsterUser(models.Model):
     # has (id), Username, password, firstname, lastname
     user = models.OneToOneField(User)
     affiliation = models.CharField(max_length=100)
 
-class Location(models.Model)
+class Location(models.Model):
     name = models.CharField(max_length=100)
 
-class Trip(models.Model)
+class Trip(models.Model):
     host = models.ForeignKey(TripsterUser)
     location = models.ForeignKey(Location)
 
-class Album(models.Model)
+class Album(models.Model):
     name = models.CharField(max_length=100)
     trip = models.ForeignKey(Trip)
 
-class Content(models.Model)
+class Content(models.Model):
     name = models.CharField(max_length=100)
     url = models.CharField(max_length=200)
     album = models.ForeignKey(Album)
 
-class FriendsWith(models.Model)
+class FriendsWith(models.Model):
     user1 = models.ForeignKey(TripsterUser)
     user2 = models.ForeignKey(TripsterUser)
 
-class ParticipantsOf(models.Model)
+class ParticipantsOf(models.Model):
     user = models.ForeignKey(TripsterUser)
     trip = models.ForeignKey(Trip)
 
-class DreamLocationOf(models.Model)
+class DreamLocationOf(models.Model):
     user = models.ForeignKey(TripsterUser)
-    name = models.CharField(max_length=100) # TODO: make this ForeignKey or leave it like this?
+    name = models.ForeignKey(Location)
 
-class TripComments
+class TripComment(models.Model):
+    user = models.ForeignKey(TripsterUser)
     trip = models.ForeignKey(Trip)
     comment = models.CharField(max_length=2000)
+    rating = models.IntegerField()
 
-class ContentComments
+class ContentComment(models.Model):
+    user = models.ForeignKey(TripsterUser)
     content = models.ForeignKey(Content)
     comment = models.CharField(max_length=2000)
+    rating = models.IntegerField()
 
-# TODO: trip rating and content rating outside of Content/Trip so other ppl can rate your content/trips?
