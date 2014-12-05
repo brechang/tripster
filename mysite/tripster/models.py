0837a56b0e7d4388997948231ca071d3c58750b7
Kevin <luhkevin@gmail.com>
2014-12-04 21:02:26 -0500

    fixed up schema

--- mysite/tripster/models.py
+++ mysite/tripster/models.py
@@ -7,13 +7,16 @@ class TripsterUser(models.Model):
     # has (id), Username, password, firstname, lastname
     user = models.OneToOneField(User)
     affiliation = models.CharField(max_length=100)
+    friends = models.ForeignKey('self')
+    dream_location = models.ForeignKey('Location')
 
 class Location(models.Model):
     name = models.CharField(max_length=100)
 
 class Trip(models.Model):
-    host = models.ForeignKey(TripsterUser)
+    host = models.OneToOneField(TripsterUser)
     location = models.ForeignKey(Location)
+    participates = models.ForeignKey(TripsterUser)
 
 class Album(models.Model):
     name = models.CharField(max_length=100)
@@ -23,18 +26,6 @@ class Content(models.Model):
     name = models.CharField(max_length=100)
     url = models.CharField(max_length=200)
     album = models.ForeignKey(Album)
-
-class FriendsWith(models.Model):
-    user1 = models.ForeignKey(TripsterUser)
-    user2 = models.ForeignKey(TripsterUser)
-
-class ParticipantsOf(models.Model):
-    user = models.ForeignKey(TripsterUser)
-    trip = models.ForeignKey(Trip)
-
-class DreamLocationOf(models.Model):
-    user = models.ForeignKey(TripsterUser)
-    name = models.ForeignKey(Location)
 
 class TripComment(models.Model):
     user = models.ForeignKey(TripsterUser)
