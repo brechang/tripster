1c4675047887c5fdd4c632a6fbcafaabea5e2e0e
Fan Yin <fanyin1234@gmail.com>
2014-12-05 20:23:23 -0500

    lkj

--- mysite/tripster/views.py
+++ mysite/tripster/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from tripster import models
+from django.contrib.auth import authentiate
 
 # Create your views here.
 
-def login(request):
+def index(request):
     return render(request, 'tripster/index.html')
 
-def auth(request):
+def register(request):
     username = request.POST['username']
     password = request.POST['password']
     affiliation = request.POST['affiliation']
-    user = User.objects.create_user(username, password = password)
+    user = User.objects.create_user(username, password=password)
     user.save()
-    t_user = models.TripsterUser(user = user, affiliation = affiliation)
+    t_user = models.TripsterUser(user=user, affiliation=affiliation)
     t_user.save()
     return render(request, 'tripster/auth.html')
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        return render(request, 'tripster/home.html')
+    else:
+        error = "Incorrect login info!"
+        return redirect('/', error=error)
