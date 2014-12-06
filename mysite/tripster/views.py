c414309faca773b71f126147d065c21f220250c8
Kevin <luhkevin@gmail.com>
2014-12-05 20:00:28 -0500

    secret santa

--- mysite/tripster/views.py
+++ mysite/tripster/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from tripster import models
 
 # Create your views here.
+
+def login(request):
+    return render(request, 'tripster/index.html')
+
+def auth(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    affiliation = request.POST['affiliation']
+    user = User.objects.create_user(username, password = password)
+    user.save()
+    t_user = models.TripsterUser(user = user, affiliation = affiliation)
+    t_user.save()
+    return render(request, 'tripster/auth.html')
